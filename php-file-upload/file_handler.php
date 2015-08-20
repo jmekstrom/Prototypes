af84a4c8b0b1a478d3497e30b3b5f10a41da0ea2
JEkstroms <jmekstrom@gmail.com>
2015-08-20 16:47:46 -0700

    prototype 1 done

--- php-file-upload/file_handler.php
+++ php-file-upload/file_handler.php
@@ -1,1 +1,6 @@
 <!-- File Handler -->
+<?php
+print_r($_FILES);
+print("<br>")
+;print_r($_POST);
+?>
--- php-file-upload/uploader_form.php
+++ php-file-upload/uploader_form.php
@@ -3,8 +3,10 @@
 <head>
 </head>
 <body>
-	<form id="file_upload">
-		
+	<form action="file_handler.php" method="POST" id="file_upload" enctype="multipart/form-data">
+		<input type="file" name="upload_file">
+		<input type="text" name="description">
+		<button>Submit</button>
 	</form>
 </body>
 </html>
