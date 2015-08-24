ccdc7f2fa68fc67daa7ccb3d946a941d2b4c77c0
JEkstroms <jmekstrom@gmail.com>
2015-08-24 12:26:46 -0700

    ajax not working yet

--- php-user-auth-basic/login_form.php
+++ php-user-auth-basic/login_form.php
@@ -1,1 +1,22 @@
 <!--login_form.php-->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+<script type="text/javascript">
+function ajaxCall(){
+	var username = $('#username').val();
+	var password = $('#password').val();
+	$.ajax({
+		url: "login_handler.php",
+		data: username,
+		cache: false,
+		method: "POST",
+		dataType: "text",
+		success: function(data){
+			console.log(data);
+		}
+	})
+}
+</script>
+
+Username:<input type="text" id="username"><br><br>
+Password :<input type="text" id="password"><br><br>
+<button type="submit" onclick="ajaxCall()">Login</button>
--- php-user-auth-basic/login_handler.php
+++ php-user-auth-basic/login_handler.php
@@ -1,1 +1,13 @@
-<!--login_handler.php-->
+
+<?php
+$user_info = [
+    ['id'=> 0, 'username'=>'dpaschal', 'password' => 'fluffybunnies'],
+    ['id'=> 0, 'username'=>'rickybobby', 'password' => 'shakeandbake'],
+    ['id'=> 0, 'username'=>'darkhelmet', 'password' => '12345'],
+    ['id'=> 0, 'username'=>'jmekstrom', 'password' => 'password'],
+    ['id'=> 0, 'username'=>'feelafal', 'password' => 'sobad']
+];
+print("Data is: ");
+print_r($_POST);
+
+?>
