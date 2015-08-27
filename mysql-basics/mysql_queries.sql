55dbcf826de6f8d18cd1a67747b91139ebc68655
JEkstroms <jmekstrom@gmail.com>
2015-08-27 14:52:36 -0700

    prototype 1 done

--- mysql-basics/mysql_queries.sql
+++ mysql-basics/mysql_queries.sql
@@ -1,1 +1,5 @@
 Place your queries below
+SELECT Users WHERE username = 'jmekstrom'
+UPDATE `Users` SET `email`='jmekstrom@gmail.com' WHERE username = 'jmekstrom'
+INSERT INTO Users SET `username` = 'Mandy', `email` = 'greatemail@email.com', `password` = sha1('thisisagreatpassword')
+DELETE FROM `Users` WHERE `username` = 'Mandy'
