4bb9f58bd09f82120ffa20b49a418c067bc82d65
JEkstroms <jmekstrom@gmail.com>
2015-08-28 17:08:32 -0700

    prototype 1 done

--- php-mysql/index_select.php
+++ php-mysql/index_select.php
@@ -1,6 +1,12 @@
 <!-- index_select.php -->
 <?php
 require('mysql_connect.php');
-
-
+$query = "SELECT * FROM todo_items WHERE user_id = '1'";
+$results = mysqli_query($conn,$query);
+if(mysqli_num_rows($results) > 0){
+   while($result = mysqli_fetch_assoc($results)){
+       $output[] = $result;
+   }
+    print_r($output);
+}
 ?>
