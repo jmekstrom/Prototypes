116ecce6cf7830ade274e4c93aeaffd9f42207af
JEkstroms <jmekstrom@gmail.com>
2015-08-28 17:27:52 -0700

    prototype 2 & 3 done

---
+++ php-mysql/form.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: JEkstroms
+ * Date: 8/28/15
+ * Time: 5:23 PM
+ */
--- php-mysql/index_insert.php
+++ php-mysql/index_insert.php
@@ -1,1 +1,12 @@
-<!-- index_insert.php -->
+<?php
+    require("mysql_connect.php");
+    $query = "INSERT INTO todo_items(id, title, details, timestamp, user_id) VALUES ('11','adding an item','no details','1440800561','1')";
+    mysqli_query($conn,$query);
+    if(mysqli_affected_rows($conn) > 0 ){
+        print_r(mysqli_affected_rows($conn)." row affected");
+    }
+    else{
+        print("no rows inserted");
+        print_r(mysqli_affected_rows($conn));
+    }
+?>
