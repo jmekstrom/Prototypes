db315cd7f8275e21805ef34248aeb34de368ccf6
JEkstroms <jmekstrom@gmail.com>
2015-08-31 11:31:38 -0700

    prototype 4 done

--- php-mysql/index_insert.php
+++ php-mysql/index_insert.php
@@ -1,12 +1,12 @@
 <?php
     print_r($_POST);
     require("mysql_connect.php");
-    $select = "SELECT * FROM todo_items WHERE user_id = '1'";
+    $select = "SELECT * FROM todo_items WHERE id=(SELECT max(id) FROM todo_items)";
     $results = mysqli_query($conn, $select);
-    $id = mysqli_num_rows($results);
-    $id = $id + 1;
+    $obj = mysqli_fetch_assoc($results);
+    $id = $obj["id"] + 1;
     print_r("<br> Current id is: $id<br>");
-    $query = "INSERT INTO todo_items(id, title, details, timestamp,priority, user_id) VALUES ('$id','$_POST[title]','$_POST[details]','$_POST[timestamp]',$_POST[priority],'1')";
+    $query = "INSERT INTO todo_items(id, title, details, timestamp,priority, user_id) VALUES ('$id','$_POST[title]','$_POST[details]','$_POST[timestamp]','$_POST[priority]','1')";
     mysqli_query($conn,$query);
     if(mysqli_affected_rows($conn) > 0 ){
         print_r(mysqli_affected_rows($conn)." row affected");
