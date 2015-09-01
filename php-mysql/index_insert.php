7fcca483f0a7823bbee6d8ae40e713eb66c5d94c
JEkstroms <jmekstrom@gmail.com>
2015-09-01 10:24:22 -0700

    nothing

--- php-mysql/index_insert.php
+++ php-mysql/index_insert.php
@@ -1,11 +1,11 @@
 <?php
-    print_r($_POST);
+    //print_r($_POST);
     require("mysql_connect.php");
     $select = "SELECT * FROM todo_items WHERE id=(SELECT max(id) FROM todo_items)";
     $results = mysqli_query($conn, $select);
     $obj = mysqli_fetch_assoc($results);
     $id = $obj["id"] + 1;
-    print_r("<br> Current id is: $id<br>");
+    //print_r("<br> Current id is: $id<br>");
     $query = "INSERT INTO todo_items(id, title, details, timestamp,priority, user_id) VALUES ('$id','$_POST[title]','$_POST[details]','$_POST[timestamp]','$_POST[priority]','1')";
     mysqli_query($conn,$query);
     if(mysqli_affected_rows($conn) > 0 ){
